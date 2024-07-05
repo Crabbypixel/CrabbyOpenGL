ca7041cc442e7841dd97b83d323497a41ef27135
Crabbyfeet <crabbypixel@gmail.com>
2024-07-06 03:20:49 +0530

    Uploaded Optimization - 1
    Added renderer class which streamlines creating and rendering objects.

---
+++ Optimization - 1/shaders/Line.glsl
@@ -0,0 +1,25 @@
+#ifdef SHADER_VERTEX
+
+layout (location = 0) in vec3 vPos;
+
+uniform mat4 matModel;
+uniform mat4 matView;
+uniform mat4 matProjection;
+
+void main()
+{
+	gl_Position = matProjection * matView * matModel * vec4(vPos, 1.0f);
+}
+#endif
+
+
+#ifdef SHADER_FRAGMENT
+
+uniform vec3 vColor;
+out vec4 FragColor;
+
+void main()
+{
+	FragColor = vec4(vColor, 1.0f);
+}
+#endif
